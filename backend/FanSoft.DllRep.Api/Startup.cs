0a564d1a326f787ac372aefa6acc1a47dd6844c7
Fabiano Nalin <fabiano.nalin@taggen.com.br>
2017-09-13 16:59:09 -0300

    Add Data

--- backend/FanSoft.DllRep.Api/Startup.cs
+++ backend/FanSoft.DllRep.Api/Startup.cs
@@ -1,18 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FanSoft.DllRep.Api.Core.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FanSoft.DllRep.Api
 {
     public class Startup
     {
+        private readonly IConfigurationRoot _configuration;
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<DLLRepositoryContext>(opt =>
+                    opt.UseSqlServer(_configuration.GetConnectionString("DLLRepositoryConn"))
+                    );
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -21,6 +35,8 @@ namespace FanSoft.DllRep.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (resp) =>
             {
