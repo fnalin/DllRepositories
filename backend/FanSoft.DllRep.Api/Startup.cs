a149a533093f3df570441f89e4640a40b1bd13b9
Fabiano Nalin <fabiano.nalin@taggen.com.br>
2017-09-13 15:11:17 -0300

    Add .net core project

---
+++ backend/FanSoft.DllRep.Api/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FanSoft.DllRep.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (resp) =>
+            {
+                resp.Response.Headers.Add("Content-Type", "text/html;charset=utf-8;");
+                await resp.Response.WriteAsync("Recurso não encontrado");
+            });
+        }
+    }
+}
